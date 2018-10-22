25158e6865966143d4c484d3b0331039c561b9b5
Leonardo Samuel Tolosa Contreras <leonardo1227@hotmail.com>
2018-10-21 20:39:09 -0500

    Revert "RestAPI DBConnection"
    This reverts commit 9b21b8ae7841f77b2bbf233594655fc67f0d97c7.

--- Applications/RestAPI/modules/dbconnection/models/index.js
+++
@@ -1,7 +0,0 @@
-const question = require("./questions");
-const user = require("./user");
-const applicationProcess = require("./applicationProcess");
-
-module.exports.question = question;
-module.exports.user = user;
-module.exports.applicationProcess = applicationProcess;
