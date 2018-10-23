995e06fc4df94f408d99921fca66322b513d8f85
Edwin Cobos <alejo740@gmail.com>
2018-10-23 17:56:27 -0500

    Implemented user creation form

--- Applications/RestAPI/modules/dbconnection/models/user.js
+++ Applications/RestAPI/modules/dbconnection/models/user.js
@@ -7,4 +7,18 @@ const schema = new mongoose.Schema({
   password: String,
   actived: Boolean
 });
+
+schema.methods.createUser = (content, callbackFunction) => {
+  mongoose
+    .model("user")
+    .insertMany(content,
+      (err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          callbackFunction(result);
+        }
+      })
+}
+
 module.exports = mongoose.model("user", schema);
--- Applications/RestAPI/routes/staff.js
+++ Applications/RestAPI/routes/staff.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const route = express.Router();
 const userCollection = require('mongoose').model('user');
+const userModel = require('../modules/dbconnection/models').user()
+const encryptation = require("../modules/encryptation");
+const cors = require("cors");
+route.use(express.json())
+route.use(cors());
 
 route.get('/', (req, res) => {
     userCollection.find((err, result) => {
         if (err) throw err;
-        
+
         res.status(201).json(result);
+    })
+});
+
+route.post('/', (req, res) => {
+    const content = req.body;
+    console.log('------------------------------------------')
+
+    content.password = encryptation.crypter(content.password);
+
+    console.log(content)
+    userModel.createUser(content, result => {
+        console.log('2222')
+        res.json({success:1});
     })
 });
 
