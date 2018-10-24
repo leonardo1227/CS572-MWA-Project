69a98137c0a44cf1ec44f05044ccf698ad53663a
Leonardo Samuel Tolosa Contreras <leonardo1227@hotmail.com>
2018-10-23 23:53:43 -0500

    Update Applications/WebClient/src/app/app.module.ts

--- Applications/WebClient/src/app/app.module.ts
+++ Applications/WebClient/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { TrackChangesDirective } from './directives/track-changes.directive';
-import { ProgressVisorComponent } from './components/progress-visor/progress-visor.component';
-import { AceEditorModule } from 'ng2-ace-editor';
-import { AceEditorComponent } from './components/exam-editor/exam-editor.component';
-import { StaffComponent } from './components/staff/staff.component';
-import { CreateStaffComponent } from './components/create-staff/create-staff.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainMaterialModule } from './modules/material.module';
-import { ExamComponent } from './components/exam/exam.component';
-import { QuestionComponent } from './components/question/question.component';
-import { QuestionEditorComponent } from './components/question-editor/question-editor.component';
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { TrackChangesDirective } from "./directives/track-changes.directive";
+import { ProgressVisorComponent } from "./components/progress-visor/progress-visor.component";
+import { AceEditorModule } from "ng2-ace-editor";
+import { AceEditorComponent } from "./components/exam-editor/exam-editor.component";
+import { StaffComponent } from "./components/staff/staff.component";
+import { CreateStaffComponent } from "./components/create-staff/create-staff.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MainMaterialModule } from "./modules/material.module";
+import { ExamComponent } from "./components/exam/exam.component";
+import { QuestionComponent } from "./components/question/question.component";
+import { QuestionEditorComponent } from "./components/question-editor/question-editor.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from "./components/login/login.component";
-import { AnswerInvitationComponent } from './components/answer-invitation/answer-invitation.component';
+import { AnswerInvitationComponent } from "./components/answer-invitation/answer-invitation.component";
 import { CreateUserComponent } from "./components/create-user/create-user.component";
 import { UsersComponent } from "./components/users/users.component";
 
@@ -30,7 +30,7 @@ import { UsersComponent } from "./components/users/users.component";
     CreateUserComponent,
     ExamComponent,
     QuestionComponent,
-    QuestionEditorComponent
+    QuestionEditorComponent,
     UsersComponent,
     LoginComponent,
     AnswerInvitationComponent
